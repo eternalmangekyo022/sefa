23282579162dd3381c9379f9036217d4d6235af3
eternalmangekyo022 <tibcso0322@gmail.com>
2022-10-12 21:35:45 +0200

    aplha build complete

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import zoli1 from '../img/zoli1.jpg';
 import zoli2 from '../img/zoli2.jpg';
 import zoli3 from '../img/zoli3.jpg';
@@ -10,16 +10,24 @@ const images: string[] = [zoli1, zoli2, zoli3];
 export default function Carousel() {
     const [index, setIndex] = useState(0);
 
+    useEffect(() => {
+        const interval = setInterval(() => setIndex(p => p === images.length - 1 ? 0 : p + 1), 5000);
+
+        return () => clearInterval(interval);
+    });
 
     return <>
-        <motion.div
-            className='absolute w-full max-w-[30rem] h-80 left-1/2 -translate-x-1/2 mt-20 overflow-hidden flex justify-center items-center rounded-3xl'
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: .7 }}
-        >
-
-            <motion.img src={images[index]} alt="" />
-        </motion.div>
+        <AnimatePresence mode='wait'>
+            <motion.div
+                className='absolute w-full max-w-[30rem] h-80 left-1/2 -translate-x-1/2 mt-20 overflow-hidden flex justify-center items-center rounded-3xl'
+                key={index}
+                initial={{ opacity: 0, left: window.innerWidth * -1 }}
+                animate={{ opacity: 1, left: window.innerWidth / 2 }}
+                exit={{ opacity: 0, left: window.innerWidth }}
+                transition={{ duration: .5 }}
+            >
+                <motion.img className='rounded-3xl' src={images[index]} alt="Zoli"/>
+            </motion.div>
+        </AnimatePresence>
     </>;
 }
