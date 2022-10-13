bd0c68d364cc533732776a7afbdf7432af319492
eternalmangekyo022 <tibcso0322@gmail.com>
2022-10-13 18:11:09 +0200

    added resizing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <>
             <div className='w-full h-full relative bg-[#e0e0e0]'>
                 <Header />
-                <h1 className='absolute left-1/2 -translate-x-1/2 mt-5 font-bold text-4xl'>Sefa</h1>
+                <h1 className='absolute left-1/2 -translate-x-1/2 mt-5 font-bold text-4xl'>♪Sefa♪</h1>
                 <Carousel />
                 <div className='w-[80%] relative left-1/2 -translate-x-1/2 top-[27rem] h-40 border-y-[1px] border-black flex justify-center items-center text-sm'>
                     <span className='w-[80%]'>Szerintem a gitározásban rejlik az élet értelme. Kiskorom óta gitározok, több
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,9 +9,13 @@ const images: string[] = [zoli1, zoli2, zoli3];
 
 export default function Carousel() {
     const [index, setIndex] = useState(0);
+    const [[wwidth, height], setSize] = useState([window.innerWidth, window.innerHeight]);
 
     useEffect(() => {
         const interval = setInterval(() => setIndex(p => p === images.length - 1 ? 0 : p + 1), 5000);
+        window.onresize = () => {
+            setSize([window.innerWidth, window.innerHeight]);
+        };
 
         return () => clearInterval(interval);
     });
@@ -21,9 +25,9 @@ export default function Carousel() {
             <motion.div
                 className='absolute w-full max-w-[30rem] h-80 left-1/2 -translate-x-1/2 mt-20 overflow-hidden flex justify-center items-center rounded-3xl'
                 key={index}
-                initial={{ opacity: 0, left: window.innerWidth * -1 }}
-                animate={{ opacity: 1, left: window.innerWidth / 2 }}
-                exit={{ opacity: 0, left: window.innerWidth }}
+                initial={{ opacity: 0, left: wwidth * -1 }}
+                animate={{ opacity: 1, left: wwidth / 2 }}
+                exit={{ opacity: 0, left: wwidth }}
                 transition={{ duration: .5 }}
             >
                 <motion.img className='rounded-3xl' src={images[index]} alt="Zoli"/>
